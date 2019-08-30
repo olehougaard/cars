d8db612f944b412ab14f3030d6241b0ac70ffaaf
= <=>
2019-08-30 08:42:29 +0200

    Introduced missing Remote interface

--- shared/src/main/java/dk/via/car_base/CarBase.java
+++ shared/src/main/java/dk/via/car_base/CarBase.java
@@ -1,10 +1,9 @@
 package dk.via.car_base;
 
-import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
-public interface CarBase extends Remote {
+public interface CarBase {
 	Car registerCar(String model, int year, Money price);
 	List<Car> getAllCars() throws RemoteException;
 	void removeCar(Car car) throws RemoteException;
